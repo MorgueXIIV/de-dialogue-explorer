d93aad155542eddce7edbe4e7ef7aa5301d4c52b
Morgan Dorian Trias <none>
2021-01-21 23:20:27 +0000

    Began converting command line program to use classes

--- dealoguesearcher.rb
+++ dealoguesearcher.rb
@@ -3,6 +3,85 @@ require 'sqlite3'
 require 'pry'
 require "colorize"
 
+class DialogueEntry
+	def initialize(convoID,lineID)
+		dialogueArray=$db.execute "SELECT id,title,dialoguetext,actor,conversant,conversationid,isgroup,hascheck,sequence,hasalts,conditionstring,userscript FROM dentries WHERE conversationid='#{convoID}' AND id='#{lineID}'";
+		dialogueArray=dialogueArray.first
+		@id=dialogueArray[0]
+		@title=dialogueArray[1]
+		@dialoguetext=dialogueArray[2]
+
+		@actorid=dialogueArray[3]
+		actorname=$db.execute "SELECT name FROM actors WHERE id='#{@actorid}'";
+		@actor=actorname[0][0]
+
+		@conversantid=dialogueArray[4]
+		actorname=$db.execute "SELECT name FROM actors WHERE id='#{@conversantid}'";
+		@conversant=actorname[0][0]
+
+		@conversationid=dialogueArray[5]
+		@isgroup=dialogueArray[6]
+		@hascheck=dialogueArray[7]
+		@sequence=dialogueArray[8]
+		@hasalts=dialogueArray[9]
+		@conditionstring=dialogueArray[10]
+		@userscript=dialogueArray[11]
+
+    end
+
+    def getParents()
+    	idsArray=$db.execute"SELECT originconversationid, origindialogueid FROM dlinks WHERE destinationconversationid='#{self.conversationid}' AND destinationdialogueid='#{self.id}'";
+    	parentsList=[]
+    	idsArray.each do |idPair|
+    		parentsList.push(DialogueEntry.new(idPair[0],idPair[1]))
+    	end
+    	@parents=parentsList
+    	return parentsList
+    end
+
+    def getChildren()
+    	idsArray=$db.execute"SELECT destinationconversationid, destinationdialogueid FROM dlinks WHERE originconversationid='#{self.conversationid}' AND origindialogueid='#{self.id}'";
+		childsList=[]
+		idsArray.each do |idPair|
+			childsList.push(DialogueEntry.new(idPair[0],idPair[1]))
+		end
+		@children=childsList
+		return childsList
+	end
+
+
+	def to_s(lomg=false)
+		if @dialoguetext=="0" then
+			stringV = "#{@actor}: #{@title}".colorize(:cyan)
+		else
+			stringV = "#{@actor}:".light_blue.bold+" #{@dialoguetext}"
+		end
+
+		if lomg
+			stringV.concat("/#{@condition}/#{@userscript}/#{@sequence}")
+		end
+
+		return stringV
+	end
+
+	def myStory(lomg=true)
+		story=""
+		story+= (@actor==0 ? "this is a hub " : "#{@actor} says #{@dialoguetext} to #{@conversant}")
+		if lomg
+			story+= @conditionstring.length>1 ? " if #{@conditionstring}" : " unconditionally" 
+			story+= @userscript.length>1 ? " and causing #{@userscript}" : " with no action"
+			story += @sequence.length>1 ? " and showing #{@sequence}" : " normally"
+		end
+		return story
+	end
+
+
+end
+
+		
+	
+# id,title,dialoguetext,actor,conversant,conversationid,isgroup,hascheck,sequence,hasalts,conditionstring,userscript
+
 def findParentLineIDs(db,convoID,lineID)
 	idsArray=db.execute"SELECT originconversationid, origindialogueid FROM dlinks WHERE destinationconversationid='#{convoID}' AND destinationdialogueid='#{lineID}'";
 	return idsArray;
@@ -34,25 +113,92 @@ def strADiaLine(lineArray)
 	end
 end
 
-#receive text input from command line
+class DialogeExplorer
+	def initialize()
+		@currentLine
+		@lineCollection=Array.new()
+		@numberOptions=Array.new()
+		choiceprocess()
+	end
+
+	def optionsAvail?()
+		if (numberOptions.nil? or numberOptions.empty?)
+			return false
+		else
+			return true
+		end
+	end
+
+	def selectedLineExists?()
+		# TODO refactor to use a method that returns true if it's a dialogue line that I'll add to that class
+		if @currentLine.nil?
+			return false
+		else 
+			return true
+		end
+	end
+
+	def choiceprocess()
+		if checkForOpts
+			puts "choose a number, or change to ... (n)ext lines, (p)revious lines, \n view (c)onversation info, (d)ump entire conversation,"
+		end
+		puts  "(s)earch for a new line) (q)uit"
+		choice = gets.chomp
+		case choice
+		when "q"
+			self.output
+			self.leave
+		when "n"
+			self.nextlines
+		when "p"
+			self.prevlines
+		when "c"
+			self.conversationinfo
+		when "d"
+			self.dialoguedump
+		when "s"
+			self.searchlines
+		else
+			optionnum=choice.to_i
+		end
+		return optionnum-1
+	end
+
+	def nextlines()
+		@currentJob="next"
+		if currentLineExists?()
+			@lineCollection.push(@currentLine)
+
+		end
+	end
+end
+
+#receive text input from command lin
 puts "Enter search query:"
 searchQ=gets.chomp
+searchQ.gsub!("'", "_")
+searchQ.gsub!('"', "_")
+
 
 begin
 	# opens a DB file to search
-	db = SQLite3::Database.open 'test.db'
+	$db = SQLite3::Database.open 'test.db'
+    db=$db
 
 	#us SQL query to get the actor name, and dialogue text from two joined tables, when they partial match the provided input string.
-	searchDias=db.execute "SELECT actors.name, dentries.dialoguetext, dentries.conversationid, dentries.id FROM dentries INNER JOIN actors ON dentries.actor=actors.id WHERE dentries.dialoguetext LIKE '%#{searchQ}%'";
+	searchDias=$db.execute "SELECT actors.name, dentries.dialoguetext, dentries.conversationid, dentries.id FROM dentries INNER JOIN actors ON dentries.actor=actors.id WHERE dentries.dialoguetext LIKE '%#{searchQ}%'";
 	#iterates over array of results, outputing the name and then dialogue
-	searchDias.each_with_index{|dia,i| puts i.to_s+": "+ strADiaLine(dia)}
+	searchobjects=[]
+	searchDias.each {|dia| searchobjects.push(DialogueEntry.new(dia[2],dia[3]))}
+	searchobjects.each_with_index{|dia,i| puts "#{i}: "+ dia.myStory(true) }
 	# asks user which of the searc results they "like"
 	puts "select a line: (q to exit)"
 	diaCollection=[];
 	lineSelector=gets.chomp;
 
+
 	while not lineSelector=="q" do
-		lineSelector=lineSelector.to_i
+		lineSelector=(lineSelector.to_i)
 		diaCollection.unshift(searchDias[lineSelector])
 		# binding.pry
 		
@@ -126,9 +272,7 @@ begin
 				puts strADiaLine(searchDias[0]).colorize(:gray)
 			elsif searchDias.length>1 then
 				puts "select a dialogue option:".colorize(:light_red)
-				searchDias.each_with_index do |dia, i|
-					puts i.to_s.colorize(:light_red) + ": " + strADiaLine(dia)
-				end
+				searchDias.each_with_index {|dia, i| puts i.to_s.colorize(:light_red) + ": " + strADiaLine(dia)}
 				lineSelector=gets.chomp;
 			else
 				"huh so I can't find the right lines sorry"
@@ -136,14 +280,15 @@ begin
 			end
 		end
 	end
-	puts "Thank You For Using the".colorize(:light_green) + "FAYDE Playback Experiment!".colorize(:yellow)
+	puts "Thank You For Using the".colorize(:light_green) + " FAYDE Playback Experiment!".colorize(:yellow)
 	#ouput the lines found one dialogue has been finished
 	diaCollection.each{|dia| puts strADiaLine(dia);}
-
-#error handling.
-rescue SQLite3::Exception => e 
-    puts "there was a Database error: " + e.to_s;
-ensure
-    # close DB, success or fail
-    db.close if db
 end
+
+# #error handling.
+# rescue SQLite3::Exception => e 
+#     puts "there was a Database error: " + e.to_s;
+# ensure
+#     # close DB, success or fail
+#     db.close if db
+# end
